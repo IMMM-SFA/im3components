2c94b40ed040c9853308ba9ef0ba72eb1938ca10
crvernon <chrisrvernon@gmail.com>
2020-11-16 11:13:00 -0500

    setup test dirs

---
+++ im3components/tests/test_gcam_cerf_expansion_plan.py
@@ -0,0 +1,26 @@
+"""Tests for the configuration reader functionality.
+
+:author:   Chris R. Vernon
+:email:    chris.vernon@pnnl.gov
+
+License:  BSD 2-Clause, see LICENSE and DISCLAIMER files
+
+"""
+
+
+import unittest
+
+import im3components as im3c
+
+
+class TestGcamCerfExpansionPlan(unittest.TestCase):
+    """Tests for the GCAM to CERF expansion plan conversion."""
+
+    def test_dummy_value(self):
+        """Test for initial dummy value return."""
+
+        # get result
+        result = im3c.gcam_cerf_expansion_plan()
+
+        # function should return 0
+        self.assertEqual(0, result, msg=f"Result for `gcam_cerf_expansion_plan` returned {result} instead of 0")
