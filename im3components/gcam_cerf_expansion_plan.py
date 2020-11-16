e290b3726e47469de17d6f9132e31cc2582c5750
crvernon <chrisrvernon@gmail.com>
2020-11-16 10:49:15 -0500

    initial function

---
+++ im3components/gcam_cerf_expansion_plan.py
@@ -0,0 +1,13 @@
+"""gcam_cerf_expansion_plan
+
+Generate an electricity capacity expansion plan from GCAM-USA in the format utilized by CERF.
+
+"""
+
+import pandas as pd
+
+
+def gcam_cerf_expansion_plan():
+    """Generate an electricity capacity expansion plan from GCAM-USA in the format utilized by CERF."""
+
+    pass
