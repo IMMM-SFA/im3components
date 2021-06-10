0885884cf57b9b6b99aac4234087e4f5e8c6fb01
Zarrar Khan <zarrar.khan@pnnl.gov>
2021-06-10 19:50:16 -0400

    Updating wrf_to_xanthos to wrf_hourly_to_monthly_mean (IN PROGRESS)

--- im3components/__init__.py
+++ im3components/__init__.py
@@ -1,4 +1,4 @@
 from im3components.gcam_cerf_expansion_plan import gcam_cerf_expansion_plan
-from im3components.wrf_to_xanthos import wrf_to_xanthos
+from im3components.wrf_hourly_to_monthly_mean import wrf_hourly_to_monthly_mean
 
-__all__ = ['gcam_cerf_expansion_plan','wrf_to_xanthos']
+__all__ = ['gcam_cerf_expansion_plan','wrf_hourly_to_monthly_mean']
