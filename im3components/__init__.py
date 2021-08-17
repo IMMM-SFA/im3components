3ce903fed40091d8802f76de15d7ef04c56d2715
Zarrar Khan <zarrar.khan@pnnl.gov>
2021-08-17 14:29:02 -0400

    Updating wrf_xanthos python scripts

--- im3components/__init__.py
+++ im3components/__init__.py
@@ -1,5 +1,4 @@
-from im3components.gcam_cerf_expansion_plan import gcam_cerf_expansion_plan
-from im3components.wrf_xanthos_to_npy import wrf_xanthos_to_npy
-from im3components.utils_replace_nan_array import utils_replace_nan_array
+from im3components import gcam_cerf_expansion_plan
+from im3components.wrf_xanthos_process import *
 
 __all__ = ['gcam_cerf_expansion_plan','wrf_xanthos_to_npy','utils_replace_nan_array']
